6113142039b84636b3ebfafb391c02469f8dafc0
AntonTimberg <antonoffice2015@gmail.com>
2024-04-04 14:11:49 +0400

    - refactor tests

--- src/test/java/AuthorServletTest.java
+++ src/test/java/AuthorServletTest.java
@@ -15,10 +15,11 @@ import java.io.PrintWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.doAnswer;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -51,42 +52,29 @@ public class AuthorServletTest {
     @Test
     void doPostWithValidAuthor() throws Exception {
         String jsonInput = "{\"name\":\"Тестовый Автор\"}";
+        Author author = new Author("Тестовый Автор");
         AuthorDTO authorDto = new AuthorDTO();
         authorDto.setName("Тестовый Автор");
-        Author author = new Author("Тестовый Автор");
 
         when(request.getReader()).thenReturn(new BufferedReader(new StringReader(jsonInput)));
-        when(toEntityConverter.convert(Mockito.any(AuthorDTO.class))).thenReturn(author);
-
-        doAnswer(invocation -> {
-            Author savedAuthor = invocation.getArgument(0);
-            savedAuthor.setId(1);
-            return null;
-        }).when(authorService).save(Mockito.any(Author.class));
+        when(toEntityConverter.convert(any(AuthorDTO.class))).thenReturn(author);
+        when(toDtoConverter.convert(any(Author.class))).thenReturn(authorDto);
 
         authorServlet.doPost(request, response);
 
         verify(authorService).save(author);
         verify(response).setStatus(HttpServletResponse.SC_CREATED);
-
         String jsonResponse = responseWriter.toString();
-        assertTrue(jsonResponse.contains("\"id\":1"));
+        assertThat(jsonResponse, containsString("\"name\":\"Тестовый Автор\""));
     }
 
     @Test
     void doGetWithoutIdReturnsAllAuthors() throws Exception {
         List<Author> authors = List.of(new Author("Автор 1"), new Author("Автор 2"));
-        List<AuthorDTO> authorDtos = authors.stream()
-                .map(a -> {
-                    AuthorDTO dto = new AuthorDTO();
-                    dto.setName(a.getName());
-                    return dto;
-                })
-                .collect(Collectors.toList());
 
         when(request.getPathInfo()).thenReturn("/");
         when(authorService.findAll()).thenReturn(authors);
-        when(toDtoConverter.convert(Mockito.any(Author.class))).thenAnswer(i -> {
+        when(toDtoConverter.convert(any(Author.class))).thenAnswer(i -> {
             Author author = i.getArgument(0);
             AuthorDTO dto = new AuthorDTO();
             dto.setName(author.getName());
@@ -96,7 +84,6 @@ public class AuthorServletTest {
         authorServlet.doGet(request, response);
 
         verify(authorService).findAll();
-        verify(response).setContentType("application/json");
 
         String jsonResponse = responseWriter.toString();
         assertTrue(jsonResponse.contains("Автор 1") && jsonResponse.contains("Автор 2"));
