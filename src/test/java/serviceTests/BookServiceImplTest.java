40c6d4141746fbd1755c48c4e8f911f96ba4e3de
AntonTimberg <antonoffice2015@gmail.com>
2024-04-13 02:58:21 +0400

    - ref

--- src/test/java/serviceTests/BookServiceImplTest.java
+++ src/test/java/serviceTests/BookServiceImplTest.java
@@ -9,12 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
