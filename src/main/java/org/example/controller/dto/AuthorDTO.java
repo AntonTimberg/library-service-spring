4a483f1559a625cfcd6faef5d43e3f0128a011bb
AntonTimberg <antonoffice2015@gmail.com>
2024-04-23 21:19:21 +0400

    -ref

--- src/main/java/org/example/controller/dto/AuthorDTO.java
+++ src/main/java/org/example/controller/dto/AuthorDTO.java
@@ -1,17 +1,9 @@
 package org.example.controller.dto;
 
-import java.util.List;
 
 public class AuthorDTO {
     private int id;
     private String name;
-//    private List<Integer> bookIds;
-//
-//    public AuthorDTO(int id, String name, List<Integer> bookIds) {
-//        this.id = id;
-//        this.name = name;
-//        this.bookIds = bookIds;
-//    }
 
     public AuthorDTO() {
     }
@@ -31,21 +23,4 @@ public class AuthorDTO {
     public void setName(String name) {
         this.name = name;
     }
-
-//    public List<Integer> getBookIds() {
-//        return bookIds;
-//    }
-//
-//    public void setBookIds(List<Integer> bookIds) {
-//        this.bookIds = bookIds;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "AuthorDTO{" +
-//                "id=" + id +
-//                ", name='" + name + '\'' +
-//                ", bookIds=" + bookIds +
-//                '}';
-//    }
 }
